f90e3b01f652fd92127c22f85dc972be1e5960b4
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-01-15 14:48:40 -0800

    add loop functions notes

---
+++ notes/16-loop-functions.R
@@ -0,0 +1,85 @@
+# Introduction ------------------------------------------------------------
+
+
+# lapply ------------------------------------------------------------------
+
+
+
+str(lapply) # function (X, FUN, ...)
+
+
+# Loop every element of X, apply FUN to every element, and return a list of the result
+# X is either a vector or list, other objects will be converted to list: as.list()
+lapply(X = 1:10, function(x) x * 10)
+lapply(X = 1:10, sqrt) # Return list of 10 elements, with sqrt of every element of X
+
+
+list.of.vec <- list(1:10, 1:20, 1:30)
+# Calculate mean for every vector in the list
+lapply(X = list.of.vec, mean)
+
+
+# Calculate mean of every column in airquality
+lapply(X = airquality, mean) # What about NA?
+lapply(X = airquality, mean, na.rm = TRUE)
+# Compare with
+colMeans(airquality, na.rm = T)
+# Difference: lapply converts the df to list before calculate the mean
+
+
+char.vec <- c('Word 1', 'Another Word', 'More Words')
+# Calculate number of chars in every vectory
+lapply(X = char.vec, nchar)
+lapply(X = list(char.vec, char.vec), nchar)
+
+
+# Find column names for these dataset: mtcars, airquality, stat.x77, CO2
+lapply(list(mtcars, airquality, state.x77, CO2), colnames)
+
+# PS. the lenght of the returned list will always be same as length of X
+
+
+
+
+# sapply ------------------------------------------------------------------
+
+# sapply is a user-friendly version and wrapper of lapply by default returning a vector, matrix or, if simplify = "array", an array if appropriate, by applying simplify2array(). sapply(x, f, simplify = FALSE, USE.NAMES = FALSE) is the same as lapply(x, f).
+
+
+# Go back and try sapply to every example obove.
+
+
+
+# vapply ------------------------------------------------------------------
+
+
+
+# replicate ---------------------------------------------------------------
+
+
+
+
+
+# apply -------------------------------------------------------------------
+
+str(apply)
+# function (X, MARGIN, FUN, ...)  
+
+
+# MARGIN = a vector giving the subscripts which the function will be applied over. 
+# MARGIN = for a matrix 1 indicates rows, 2 indicates columns, c(1, 2) indicates rows and columns. 
+# MARGIN = dim(X)
+
+apply(1:30, 1, mean) # Error: dim(X) must have a positive length
+dim(1:30) # NULL
+
+mat <- matrix(1:30) # matrix here is one column
+apply(mat, 1, sqrt) # Apply sqrt() to every row
+apply(mat, 2, sqrt) # Apply sqrt() to every col
+
+sqrt(1:2)
+
+
+
+
+
