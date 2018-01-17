5482044c5331c08c528c7d2e7c25acea73ecfe02
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-01-16 20:44:03 -0800

    update notes/16-loop-functions.R

--- notes/16-loop-functions.R
+++ notes/16-loop-functions.R
@@ -1,5 +1,17 @@
 # Introduction ------------------------------------------------------------
 
+"
+I divide the `apply family` functions into three groups:
+1) lapply, sapply, vapply, ...
+2) tapply and split
+3) apply
+
+"
+
+
+# lapply, sapply, vapply, ... ---------------------------------------------
+
+
 
 # lapply ------------------------------------------------------------------
 
@@ -8,17 +20,17 @@
 str(lapply) # function (X, FUN, ...)
 
 
-# Loop every element of X, apply FUN to every element, and return a list of the result
+# Loop every element of X, apply FUN to every element, and return `a list` of the result
 # X is either a vector or list, other objects will be converted to list: as.list()
 lapply(X = 1:10, function(x) x * 10)
 lapply(X = 1:10, sqrt) # Return list of 10 elements, with sqrt of every element of X
 
-
 list.of.vec <- list(1:10, 1:20, 1:30)
 # Calculate mean for every vector in the list
 lapply(X = list.of.vec, mean)
-
-
+View(mtcars)
+# What the returned value?
+lapply(mtcars, mean)
 # Calculate mean of every column in airquality
 lapply(X = airquality, mean) # What about NA?
 lapply(X = airquality, mean, na.rm = TRUE)
@@ -40,7 +52,6 @@ lapply(list(mtcars, airquality, state.x77, CO2), colnames)
 
 
 
-
 # sapply ------------------------------------------------------------------
 
 # sapply is a user-friendly version and wrapper of lapply by default returning a vector, matrix or, if simplify = "array", an array if appropriate, by applying simplify2array(). sapply(x, f, simplify = FALSE, USE.NAMES = FALSE) is the same as lapply(x, f).
@@ -59,6 +70,41 @@ lapply(list(mtcars, airquality, state.x77, CO2), colnames)
 
 
 
+# Part 2: tapply and split ------------------------------------------------
+
+# split and unsplit
+?split
+str(split) # function (x, f, drop = FALSE, ...)  
+
+# Split mtcars by cyl
+str(split(mtcars, mtcars$cyl))
+# Split airquality by month
+str(split(mtcars, airquality$Month))
+
+as.factor(mtcars$cyl)
+str(as.factor(mtcars$cyl))
+
+
+# Let's load the patients.data data.frame
+load('data/r-novice-inflammation/patients.data.rda')
+
+split(patients.data, patients.data$group)
+
+
+
+# tapply: "https://www.r-bloggers.com/r-function-of-the-day-tapply-2/"
+?tapply
+
+tapply(mtcars$mpg, mtcars$cyl, mean)
+tapply(airquality$Ozone, airquality$Month, mean, na.rm = T)
+
+
+tapply(patients.data$bloodpressure, list(patients.data$gender, patients.data$group), median)
+
+
+## contingency table from data.frame : array with named dimnames
+tapply(warpbreaks$breaks, warpbreaks[,-1], sum)
+tapply(warpbreaks$breaks, warpbreaks[, 3, drop = FALSE], sum)
 
 # apply -------------------------------------------------------------------
 
